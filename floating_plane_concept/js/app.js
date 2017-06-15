a509027aaf6e6be6ded7748eeb5eb8dbf7ff09b3
Riccardo Marotti <riccardo.marotti@gmail.com>
2017-06-15 16:27:01 +0200

    Add player/cylinder collision

--- floating_plane_concept/js/app.js
+++ floating_plane_concept/js/app.js
@@ -14,11 +14,11 @@ var createScene = function () {
     var engine3D = new BABYLON.Engine(canvas3D, true);
     var scene3D = new BABYLON.Scene(engine3D);
 
-    var camera3D = new BABYLON.ArcRotateCamera("ArcRotateCamera", 
-                                                -0.7, 
-                                                0.8, 
+    var camera3D = new BABYLON.ArcRotateCamera("ArcRotateCamera",
+                                                -0.7,
+                                                0.8,
                                                 10,
-                                                new BABYLON.Vector3(0, 0, 0), 
+                                                new BABYLON.Vector3(0, 0, 0),
                                                 scene3D);
     camera3D.attachControl(canvas3D, true, true , true);
     camera3D.inputs.remove(camera3D.inputs.attached.keyboard);
@@ -27,7 +27,7 @@ var createScene = function () {
     light3D.intensity = 0.7;
 
     var cylinder3D = BABYLON.Mesh.CreateCylinder("cylinder", 6, 2, 2, 50, 1, scene3D, false);
-    var plane3D = BABYLON.MeshBuilder.CreateBox("box", {size: 5, width: 0.1}, scene3D);
+    var plane3D = BABYLON.MeshBuilder.CreateBox("box", {size: 5, width: 0.02}, scene3D);
 
 
     var planeMaterial = new BABYLON.StandardMaterial("transparent", scene3D);
@@ -36,10 +36,11 @@ var createScene = function () {
 
     planeMaterial.diffuseColor = planeColor;
     plane3D.material = planeMaterial;
-    
+
     var canvas2D = document.getElementById('canvas2D');
     var engine2D = new BABYLON.Engine(canvas2D, true);
     var scene2D = new BABYLON.Scene(engine2D);
+
     var light2D = new BABYLON.HemisphericLight("light2D", new BABYLON.Vector3(1, 1, 1), scene2D);
     light2D.intensity = 1;
 
@@ -51,16 +52,16 @@ var createScene = function () {
     player.rotation.z = Math.PI/2;
     player.position.z = -2;
 
-    var camera2D = new BABYLON.ArcRotateCamera("ArcRotateCamera", 
-                                                0, 
-                                                1.5, 
-                                                10, 
-                                                new BABYLON.Vector3(0, 0, 0), 
+    var camera2D = new BABYLON.ArcRotateCamera("ArcRotateCamera",
+                                                0,
+                                                1.5,
+                                                10,
+                                                new BABYLON.Vector3(0, 0, 0),
                                                 scene2D
                                                );
 
-
-    camera2D.attachControl(canvas2D, true, true , true);                                            
+    camera2D.attachControl(canvas2D, true, true , true);
+    camera2D.inputs.remove(camera2D.inputs.attached.keyboard);
     engine3D.runRenderLoop(function () {
         scene3D.render();
     });
@@ -72,32 +73,41 @@ var createScene = function () {
         plane3D.position.x -= movementForKeycode3D[KEY_A] || 0;
         player.position.x -= movementForKeycode3D[KEY_A] || 0;
         camera2D.target.x -= movementForKeycode3D[KEY_A] || 0;
-        
-        player.position.z -= movementForKeycode2D[KEY_LEFT] || 0;
-        player.position.y -= movementForKeycode2D[KEY_DOWN] || 0;
 
         plane3D.position.x += movementForKeycode3D[KEY_D] || 0;
         player.position.x += movementForKeycode3D[KEY_D] || 0;
         camera2D.target.x += movementForKeycode3D[KEY_D] || 0;
 
-        player.position.z += movementForKeycode2D[KEY_RIGHT] || 0;
-        player.position.y += movementForKeycode2D[KEY_UP] || 0;
-
         var plane2Dtemp = BABYLON.CSG.FromMesh(plane3D);
         var cylinder2Dtemp = BABYLON.CSG.FromMesh(cylinder3D);
         var intersection =  plane2Dtemp.intersect(cylinder2Dtemp);
-        
-        var cylinder2D = intersection.toMesh("intersection", cylynder2DMaterial, scene2D);
-        
+
+        var cylinder2D = null;
+        if(cylinder3D.intersectsMesh(plane3D)){
+            cylinder2D = intersection.toMesh("intersection", cylynder2DMaterial, scene2D);
+        }
+
         var planeMaterial2D = new BABYLON.StandardMaterial("plane2DMaterial", scene2D);
         planeMaterial2D.diffuseColor = new BABYLON.Color3(0.5, 1, 1);
         var plane2D = plane2Dtemp.toMesh("plane", planeMaterial2D, scene2D);
         plane2D.position.x -= 0.01;
-        
+
+        player.position.z -= movementForKeycode2D[KEY_LEFT] || 0;
+        player.position.y -= movementForKeycode2D[KEY_DOWN] || 0, 0;
+        player.position.z += movementForKeycode2D[KEY_RIGHT] || 0;
+        player.position.y += movementForKeycode2D[KEY_UP] || 0;
+
+        if(cylinder2D && cylinder2D.intersectsMesh(player, false)) {
+            player.position.z = -2;
+            player.position.y = 0;
+        }
+
         scene2D.render();
 
         plane2D.dispose();
-        cylinder2D.dispose();
+        if (cylinder2D) {
+            cylinder2D.dispose();
+        }
     });
 
     window.addEventListener('resize', function () {
